53335ab668468259f6ac54800d9313159ebb61da
Kirill Makhnev <kalorigen@gmail.com>
2023-05-26 14:42:09 +0300

    fixes

--- src/modules/Contacts/Contacts.tsx
+++ src/modules/Contacts/Contacts.tsx
@@ -3,12 +3,20 @@ import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import style from './Contacts.module.scss';
 import { ContactsText } from './ContactsText';
 import Input from '../../components/Input/Input';
+import { ImgWebp } from '../../helpers/imgwebp';
+
+import img from './assets/image.png';
+import img2x from './assets/image@2x.png';
+import imgWebp from './assets/image.webp';
 
 const Contacts = () => {
   return (
     <section id='contact' className={style.contacts}>
       <div className='container'>
         <div className={style.contactsBody}>
+          <div className={style.contactsBody__img}>
+            <ImgWebp src={img} src2x={img2x} srcWebp={imgWebp} />
+          </div>
           <div className={style.contactsBody__content}>
             <SectionTitle className={style.contactsBody__title}>
               {ContactsText.title}
